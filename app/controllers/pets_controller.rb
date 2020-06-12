cd64ce78a4da13a87373c6d198d8e58209ee3ec1
Jozy Kinnaman <jozypants@gmail.com>
2020-06-12 09:30:30 -0700

    add methods to controllers for testing and making API calls.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,35 @@
 class PetsController < ApplicationController
-  
+  def index
+    @pets = Pet.all
+    json_response(@pets)
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+    json_response(@pet)
+  end
+
+  def create
+    @pet = Pet.create(pet_params)
+    json_response(@pet)
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def pet_params
+    params.permit(:breed, :age, :gender, :desc, :name, :species)
+  end
 end
