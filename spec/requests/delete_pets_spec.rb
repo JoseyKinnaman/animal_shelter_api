15cd27973743d658ef72f499ca03d33167ad2b40
Jozy Kinnaman <jozypants@gmail.com>
2020-06-12 11:47:58 -0700

    update test for delete route. Expect pass.

--- spec/requests/delete_pets_spec.rb
+++ spec/requests/delete_pets_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 describe "delete pets route", :type => :request do
-  let!(:pets) { FactoryBot.create_list(:pet, 20)}
 
-  before { delete '/pets/20'}
-
-  it 'returns status code 200' do
-    expect(response).to have_http_status(:success)
+  it "will delete a pet entry" do
+    new_pet = Pet.create!({ :breed => 'test_breed', :age => 5, :gender => 'female', :desc => 'A good boi', :name => 'Hank', :species => 'dog'})
+    delete "/pets/#{new_pet.id}"
+    expect(response).to have_http_status(:200)
   end
-  
+
 end
